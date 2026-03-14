a92e75599f4d21b5084e3c141724cfd81b507ebe
kwtSMS Admin <support@kwtsms.com>
2026-03-14 07:29:18 +0300

    Fix CI: disable eslint ban-ts-comment for session storage workaround

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -14,6 +14,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore Duplicate @shopify/shopify-api versions cause type mismatch
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
